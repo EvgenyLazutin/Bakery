a88a8cc915fe901b31779358512ef2f9bf500642
Лазутин Евгений <e_lazutin@mail.ru>
2019-05-10 01:19:37 +0300

    Added input reader and test.

---
+++ src/test/java/service/OrderReaderTest.java
@@ -0,0 +1,37 @@
+package service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrderReaderTest {
+
+    private OrderReader orderReader;
+
+    @Before
+    public void init() {
+        orderReader = new OrderReader();
+    }
+
+    @After
+    public void tearDown() {
+        orderReader = null;
+    }
+
+    @Test
+    public void orderReaderTest() throws Exception {
+        List<String> result = orderReader.readFile("order.txt");
+        assertEquals(3, result.size());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void orderReaderTest2() throws Exception {
+        List<String> result = orderReader.readFile("fileDoesNotExist.txt");
+        assertEquals(3, result.size());
+    }
+
+}
